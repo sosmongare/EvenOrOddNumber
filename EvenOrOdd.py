248c1aec7f6b5fb97503ae237a4f91ca46adf812
Sospeter Mongare <sosmongare@gmail.com>
2022-04-19 10:46:18 +0300

    Comment added on every if...else statement

--- EvenOrOdd.py
+++ EvenOrOdd.py
@@ -3,7 +3,9 @@
 # If the remainder is 1, it is an odd number.
 
 num = int(input("Enter a number: "))
+# A number is even if division by 2 gives a remainder of 0.
 if (num % 2) == 0:
    print("{0} is Even".format(num))
+ # If the remainder is 1, it is an odd number.  
 else:
    print("{0} is Odd".format(num))
